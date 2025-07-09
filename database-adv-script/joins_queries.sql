99c572909d6abfa9d0afc12ab27f946bdaeb841a
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-09 19:16:36 +0100

    Fix LEFT JOIN query with ORDER BY for ALX checker

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,3 +1,5 @@
-SELECT properties.*, reviews.*
+-- Task: Retrieve all properties and their reviews (including properties with no reviews)
+SELECT properties.id AS property_id, properties.name, reviews.id AS review_id, reviews.comment
 FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
+LEFT JOIN reviews ON properties.id = reviews.property_id
+ORDER BY properties.id;
