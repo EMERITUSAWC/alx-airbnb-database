6a2dd788859d19a594d76131fa0ef056156d85e8
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-09 18:41:06 +0100

    Fix directory and ensure correct file structure

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,4 +1,4 @@
--- Query to retrieve all properties and their reviews (including properties with no reviews)
-SELECT p.*, r.*
-FROM properties p
-LEFT JOIN reviews r ON p.id = r.property_id;
+-- LEFT JOIN: Get all properties and their reviews, including properties with no reviews
+SELECT properties.id, properties.name, reviews.comment
+FROM properties
+LEFT JOIN reviews ON properties.id = reviews.property_id;
