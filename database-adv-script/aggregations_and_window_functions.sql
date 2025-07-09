ec5419f41342ad927560ee2a8fc7ad1d6b5b6a17
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-09 20:11:19 +0100

    Use ROW_NUMBER() to rank properties by booking count

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -1,5 +1,13 @@
--- 2. Rank properties based on total number of bookings using RANK()
-SELECT property_id, COUNT(*) AS total_bookings,
-       RANK() OVER (ORDER BY COUNT(*) DESC) AS booking_rank
+-- 1. Total number of bookings made by each user
+SELECT user_id, COUNT(*) AS total_bookings
 FROM bookings
-GROUP BY property_id;
+GROUP BY user_id
+ORDER BY total_bookings DESC;
+
+-- 2. Rank properties based on number of bookings using ROW_NUMBER()
+SELECT property_id,
+       COUNT(*) AS total_bookings,
+       ROW_NUMBER() OVER (ORDER BY COUNT(*) DESC) AS rank
+FROM bookings
+GROUP BY property_id
+ORDER BY total_bookings DESC;
