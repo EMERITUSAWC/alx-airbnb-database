5e9380865c826ea7ef8f58d90dc0508107718a96
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-09 18:39:08 +0100

    Fix directory structure and ensure correct files

--- database-adv-script/database-adv-script/joins_queries.sql
+++
@@ -1,4 +0,0 @@
--- LEFT JOIN: Get all properties and their reviews, including properties with no reviews
-SELECT properties.id, properties.name, reviews.comment
-FROM properties
-LEFT JOIN reviews ON properties.id = reviews.property_id;
